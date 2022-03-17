1bbe46f8c2a5963f980031779231ce4669f14433
aleeixoj <aleeixojunior2016@hotmail.com>
2022-03-17 20:10:02 -0300

    Alteração de foto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,9 @@ const Home: NextPage = () => {
             }}
           >
             <Image
-              src={'/favicon.jpg'}
+              src={
+                'https://lh3.googleusercontent.com/EobSKBkhU9IIiTyR4V2I3EdeTpM9mBnop66mnKkFgib2LvyadPeDBPgjGZ8jCja8svqWnPFI7eeLGU84ayd9oIsdYRiFtBQgx0EeVo73uuoGKS8n-8ocnuc-DvcDCaRYNVUCsGpUFI5IfmEqyl7tfKOpIem3qAoLW1E9j1PY2vBrRHYVpoDO61R_X5ZmSbUPVI3z9oWX751lei0DZ33t6UlbMYPmFqj0s24zcYj42ZJ6hi6mVB0rGjFVTd2evZIAXY7xRu1nFDzmd4rpbBwtxVc5cBmiSBLojypi3kd28IQFTL_bKLnStPHgOjHFOz0gOwt2B7Y_95WiQicdNmuxB4Am9xByWiBkLrttIC2iVuGrkeuVpwUtVTpXHQ506EvwKd2YT0FZa2BlCON6-R2EKDG9G90dCdcONxrOOoKWO5pfVPScnNbj5WybZiAnFb-3gz7Q6FAsnpYGjz4Ug_fy0Frk86RgRkFpbWbFOPOYn2jgniBL6p8s8mfGonZSfwNaAsg_c2d5ZTuAXzR7HT3IufI4xlTZ8dZGKQfRDXqW-nLz4SzE3RQrWU2M4-xluJvwVI4ZyxLLNm0NkOqdfP84TWohf7ZKeYBj-cE8qgke5A_V2OHbmkVHadxazf5ZJzmLsvw3NX2Jv-uDLP34kYsVe-_FeXNon-uPN66PBew5e6kdmzCNAG1i_umg410fkvmE49LzlsoT6QnbakbVbWByMDyk=w999-h980-no?authuser=0'
+              }
               objectFit={'cover'}
               boxSize="15rem"
               borderRadius="full"
