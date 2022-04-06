a4cfcfe8f5e5036b85f3f072c9dd72fe51bec780
aleeixoj <aleeixojunior2016@hotmail.com>
2022-04-06 08:01:42 -0300

    Ajuste de data

--- src/pages/pool/index.tsx
+++ src/pages/pool/index.tsx
@@ -245,7 +245,7 @@ const Pool: NextPage = () => {
             >
               <Text color={'red.600'}>
                 Precisamos que a sua presença seja confirmada até dia
-                05/04/2022, para que possamos nos organizar. Após esse prazo o
+                10/04/2022, para que possamos nos organizar. Após esse prazo o
                 link de confirmação não estará mais disponível.
               </Text>
             </Box>
